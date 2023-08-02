25e0725ece320a16151e298ada08b0f7f27cfa83
Krishna Thoriya <70200639+Krishna0312@users.noreply.github.com>
2023-08-02 12:27:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using linqApp5;
+using System.Xml.Linq;
+
+namespace linqApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("d:\\youtube.csv");
+
+            int length = lines.Count() - 1;
+            Console.WriteLine($"Count is {length}");
+
+            //var r1 = lines.Distinct();
+        }
+        
+    }
+}
